749605fda9fe795b9431b9359c5eb33ddae513e2
Bartek <bart.matusz@gmail.com>
2020-10-11 01:14:20 +0200

    configured dependencies to use kotlin serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
+buildscript {
+    val kotlinVersion = "1.3.61"
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
+    dependencies {
+        classpath(kotlin("gradle-plugin", kotlinVersion))
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+    }
+}
+
 plugins {
     java
     kotlin("jvm") version "1.3.61"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.3.61"
 }
 
 group = "org.example"
@@ -12,6 +25,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.14.0")
     testCompile("junit", "junit", "4.12")
 }
 
--- src/main/kotlin/entities/MeetingEntity.kt
+++ src/main/kotlin/entities/MeetingEntity.kt
@@ -1,5 +1,7 @@
 package com.rsk.entities
 
+import kotlinx.serialization.Serializable
+@Serializable
 class MeetingEntity(val id: Int,
                     val meetingName: String,
                     val location: LocationEntity,
@@ -8,13 +10,13 @@ class MeetingEntity(val id: Int,
         return "MeetingEntity(id=$id, meetingName='$meetingName', location=$location, participants=$participants)"
     }
 }
-
+@Serializable
 class ParticipantEntity(val name: String) {
     override fun toString(): String {
         return "ParticipantEntity(name='$name')"
     }
 }
-
+@Serializable
 class LocationEntity(val roomName: String) {
     override fun toString(): String {
         return "LocationEntity(name='$roomName')"
